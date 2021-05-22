7f0264317d2ca411284a11a328e94f2d932ce6f1
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-05-22 18:55:24 +0300

    dele

--- src/components/CustomMarker/CustomMarker.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-import {CustomMarkerPinkStyled, CustomMarkerBlueStyled} from "./styles"
-
-const Pink = 'pink';
-export const CustomMarker = ({type}) => type === Pink ? <CustomMarkerPinkStyled /> : <CustomMarkerBlueStyled />
