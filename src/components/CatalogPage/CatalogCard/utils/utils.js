a696ac15e0ba080ac60a965ad87ba2cbd4065eca
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-05-25 13:03:04 +0300

    shop: update success link

--- src/components/CatalogPage/CatalogCard/utils/utils.js
+++ src/components/CatalogPage/CatalogCard/utils/utils.js
@@ -1,7 +1,7 @@
 
 export const getPaylink = (order_id, customer_phone, customer_email, products, paid_content) => {
 
-  let link = `https://demidova.payform.ru/?order_id=${order_id}&customer_phone=${customer_phone}&customer_email=${customer_email}&products[0][price]=${products[0].price.split(' ').join('')}&products[0][quantity]=1&products[0][name]=Обучающие материалы: ${products[0].name}&customer_extra=Полная оплата курса&paid_content=${paid_content}&urlSuccess=https://astrologdemidova.ru/shop&do=pay`;
+  let link = `https://demidova.payform.ru/?order_id=${order_id}&customer_phone=${customer_phone}&customer_email=${customer_email}&products[0][price]=${products[0].price.split(' ').join('')}&products[0][quantity]=1&products[0][name]=Обучающие материалы: ${products[0].name}&customer_extra=Полная оплата курса&paid_content=${paid_content}&urlSuccess=https://shop.astrologdemidova.ru&do=pay`;
 
   // let response = customer_phone && customer_email ? await fetch(link) : null;
   // let shotLink;
