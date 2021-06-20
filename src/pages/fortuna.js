c0c774d399c3f5b780ba6f35860f1ba89a97da25
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-06-20 19:42:30 +0300

    update bg

--- src/pages/fortuna.js
+++ src/pages/fortuna.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Helmet } from "react-helmet";
 import { FortunaTextWinner } from '../components/FortunaTextWinner';
 import { FortunaWheel } from '../components/FortunaWheel';
-import {CatalogFooter} from '../components/CatalogPage/CatalogFooter';
+import { CatalogFooter } from '../components/CatalogPage/CatalogFooter';
 
 const Fortuna = () => {
     const [typePrize, setType] = React.useState();
@@ -10,10 +10,25 @@ const Fortuna = () => {
     return (
         <>
             <Helmet title='Колесо Фортуны от Яны Демидовой' />
-            
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '50px'}}>
-                <FortunaWheel setType={setType} />
-                <FortunaTextWinner typePrize={typePrize} />
+
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+
+            }}>
+                <div style={{
+                    width: '100%',
+                    padding: '50px 0 100px',
+                    background: 'rgb(63,94,251)',
+                    background: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(14,4,128,1) 100%)'
+                }}>
+                    <FortunaWheel setType={setType} />
+                    <FortunaTextWinner typePrize={typePrize} />
+                </div>
+                
+              
                 <CatalogFooter />
             </div>
         </>
