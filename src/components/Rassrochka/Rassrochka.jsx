d1cc0c849e05ae0613d941056d76079b076a9dfa
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-06-20 17:02:45 +0300

    dev: add rassrochka, garant 100%,

---
+++ src/components/Rassrochka/Rassrochka.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { CustomButton } from '../CustomButton';
+import { Wrapper } from './styles';
+
+export const Rassrochka = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    return (
+        <Wrapper>
+            <h4 onClick={() => setIsVisible(!isVisible)}>Если Тинькофф банк не одобрил вам рассрочку &#128072;</h4>
+
+            {isVisible && (
+                <>
+                    <p>
+                        Я позаботилась и подготовила для вас ещё одну возможность перейти на новый финансовый уровень.
+                        Эта возможность- рассрочка через другие банки. Все, что вам нужно - просто заполнить анкету со своими данными.
+                    </p>
+                    <p>
+                        *эта анкета будет отправляться сразу в несколько банков, для того , чтобы в разы увеличить вероятность одобрения рассрочки для вас
+                    </p>
+                    <p>
+                        Заполняйте анкету и начинаем ваш переход на новый уровень жизни и дохода! До встречи в марафоне !
+                    </p>
+
+                    <div>
+                        <a href='#' target='_blank'>
+                            <CustomButton>
+                                Заявка на рассрочку
+                            </CustomButton>
+                        </a>
+                    </div>
+                </>
+            )}
+
+        </Wrapper>
+    )
+}
---
+++ src/components/Rassrochka/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    padding-top: 40px;
+    padding-bottom: 40px;
+    margin: 0 auto;
+    max-width: 768px;
+    width: 100%;
+    h4 {
+        cursor: pointer;
+        text-decoration: underline;
+        color: #f75995;
+    }
+    div {
+        margin-top: 60px;
+    }
+`;
