3856fc16d46bfec0d4ac22d461b589b9afb1077c
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-06-25 12:53:30 +0300

    update getpayLink

--- src/components/CatalogPage/CatalogCard/utils/utils.js
+++ src/components/CatalogPage/CatalogCard/utils/utils.js
@@ -1,18 +1,25 @@
-
 export const getPaylink = (order_id, customer_phone, customer_email, products, paid_content) => {
+  let orderId = `order_id=${order_id}&`;
+  let customerPhone = `customer_phone=${customer_phone}&`;
+  let customerEmail = `customer_email=${customer_email}&`;
+  let productsArr = `products[0][price]=${products[0].price.split(' ').join('')}&products[0][quantity]=1&products[0][name]=Обучающие материалы: ${products[0].name}&`;
+  let customerExtra = `customer_extra=Полная оплата курса&`;
+  let paidContent = `paid_content=${paid_content}&`;
+  let urlSuccess = `urlSuccess=https://shop.astrologdemidova.ru&`;
+  let dO = `do=pay`
 
-  let link = `https://demidova.payform.ru/?order_id=${order_id}&customer_phone=${customer_phone}&customer_email=${customer_email}&products[0][price]=${products[0].price.split(' ').join('')}&products[0][quantity]=1&products[0][name]=Обучающие материалы: ${products[0].name}&discount_value=49&customer_extra=Полная оплата курса&paid_content=${paid_content}&urlSuccess=https://shop.astrologdemidova.ru/fortuna`;
+  let discountValue = 'discount_value=49&'
 
-  // let response = customer_phone && customer_email ? await fetch(link) : null;
-  // let shotLink;
-
-  // if (response.ok) {
-  //   shotLink = await response.text();
-  //   console.log('###', shotLink);
-  // } else {
-  //   console.log("Ошибка HTTP: " + response.status);
-  // }
-
+  let link = 'https://demidova.payform.ru/?' +
+    orderId +
+    customerPhone +
+    customerEmail +
+    productsArr +
+    discountValue+
+    customerExtra +
+    paidContent +
+    urlSuccess +
+    dO;
 
   return link;
 }
