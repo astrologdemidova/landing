417eb4d71be606d1501f2636c21f965c0c8d0a8f
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-06-26 23:48:48 +0300

    up

--- src/components/TariffBlock/TariffBlock.jsx
+++ src/components/TariffBlock/TariffBlock.jsx
@@ -32,7 +32,7 @@ export const TariffBlock = () => {
     let date2 = new Date("2021-06-22T22:34:00.998+0300");
     
     //const step = dateCurrent < date2 ? '1' : '2'; // change it 1-2-3
-    const step = '1'; // change it 1-2-3
+    const step = '2'; // change it 1-2-3
     const data = [
         {
             id: '1112',
