f7249182687b030376762825dbef35b5d2aec838
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-06-30 20:22:21 +0300

    update

--- src/components/CatalogPage/CatalogCard/utils/utils.js
+++ src/components/CatalogPage/CatalogCard/utils/utils.js
@@ -1,5 +1,31 @@
 import { nanoid } from 'nanoid';
 
+const checkWin = nanoid();
+export const getPaylink = (order_id, customer_phone, customer_email, products, paid_content) => {
+  let orderId = `order_id=${order_id}&`;
+  let customerPhone = `customer_phone=${customer_phone}&`;
+  let customerEmail = `customer_email=${customer_email}&`;
+  let productsArr = `products[0][price]=${products[0].price.split(' ').join('')}&products[0][quantity]=1&products[0][name]=Обучающие материалы: ${products[0].name}&`;
+  let customerExtra = `customer_extra=Полная оплата курса&`;
+  let paidContent = `paid_content=${paid_content}&`;
+  let urlSuccess = `urlSuccess=https://shop.astrologdemidova.ru/fortuna?che=${checkWin}&cust_email=${customer_email}&cust_phone=${customer_phone}&`;
+  let dO = `do=pay`
+
+  let link = 'https://demidova.payform.ru/?' +
+    orderId +
+    customerPhone +
+    customerEmail +
+    productsArr +
+    customerExtra +
+    paidContent +
+    urlSuccess +
+    dO;
+  return link;
+}
+
+/* рабочий вариант
+import { nanoid } from 'nanoid';
+
 export const getPaylink = (order_id, customer_phone, customer_email, products, paid_content) => {
   let checkWin = nanoid();
   let orderId = `order_id=${order_id}&`;
@@ -23,3 +49,4 @@ export const getPaylink = (order_id, customer_phone, customer_email, products, p
 
   return link;
 }
+*/
